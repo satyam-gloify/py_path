c1cc279a2a56b4e2a92c879296a006810743a31f
Oo oO <oooo@Oos-MacBook-Pro.local>
2024-11-29 01:25:01 +0530

    fabonachi

--- 2022/basic/keywor/generator.py
+++ 2022/basic/keywor/generator.py
@@ -1,10 +1,45 @@
-# 2 second time and 3 third time
-def simpleGeneratorFun():
-    yield 1            
-    yield 2            
-    yield 3            
+## 2 second time and 3 third time
+# def simpleGeneratorFun():
+#     yield 1            
+#     yield 2            
+#     yield 3            
    
-# Driver code to check above generat
-print(simpleGeneratorFun())
-for v in simpleGeneratorFun():
-    print(v)
+# # Driver code to check above generat
+# print(simpleGeneratorFun())
+# for v in simpleGeneratorFun():
+#     print(v)
+
+
+
+    ##########
+
+# A simple generator for Fibonacci Numbers
+def fib(limit):
+      
+    # Initialize first two Fibonacci Numbers 
+    a, b = 0, 1
+  
+    # One by one yield next Fibonacci Number
+    while a < limit: #0<5
+        yield a #0
+        a, b = b, a + b #a(1)b(1) = 1|,0+1 #a(1),b(1) = 1|, 1 + 1 #a(1),b(2) = 2|, 1+2 #a(2) b(5) = 3|,3+2 
+
+  
+# Create a generator object
+x = fib(9)
+  
+# Iterating over the generator object using next
+# print(x.next()) # In Python 3, __next__()
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+
+# Iterating over the generator object using for
+# in loop.
+print("\nUsing for in loop")
+# for i in fib(5):
+#     print(i)
